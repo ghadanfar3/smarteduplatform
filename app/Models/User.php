93e33a694905c9f9ab05e4461188cecfbb4d00f4
ghadanfar3 <ghadanfarheusen8@gmail.com>
2025-11-06 10:08:07 +0300

    Renamed course.php to Course.php for consistency

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,15 @@
 
 namespace App\Models;
 
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+     use HasApiTokens, HasFactory, Notifiable;
 
     protected $fillable = [
         'name',
@@ -41,7 +41,8 @@ class User extends Authenticatable
         ];
     }
 
-    public function courses() {
+    public function courses()
+    {
         return $this->hasMany(Course::class, 'teacher_id');
     }
 
--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
-    return "welcom from api";
+    return "welcome from api";
 })->middleware('auth:sanctum');
 Route::post('/login', [AuthController::class, 'login'] )->name("login");
-Route::post('/register', [AuthController::class, 'store'] );
+Route::post('/register', [AuthController::class, 'store'] )->name("register");
+
